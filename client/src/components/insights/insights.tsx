f3fb7df9af681f78a7f5e9a1216de7f8c7814e7d
Sean Outhwaite <sean.outhwaite@gmail.com>
2026-01-18 12:19:46 +1300

    Fix get insights API response error and update insight key names so they render

--- client/src/components/insights/insights.tsx
+++ client/src/components/insights/insights.tsx
@@ -1,40 +1,40 @@
-import { Trash2Icon } from "lucide-react";
-import { cx } from "../../lib/cx.ts";
-import styles from "./insights.module.css";
-import type { Insight } from "../../schemas/insight.ts";
+import { Trash2Icon } from 'lucide-react'
+import { cx } from '../../lib/cx.ts'
+import styles from './insights.module.css'
+import type { Insight } from '../../schemas/insight.ts'
 
 type InsightsProps = {
-  insights: Insight[];
-  className?: string;
-};
+  insights: Insight[]
+  className?: string
+}
 
 export const Insights = ({ insights, className }: InsightsProps) => {
-  const deleteInsight = () => undefined;
+  const deleteInsight = () => undefined
 
   return (
     <div className={cx(className)}>
       <h1 className={styles.heading}>Insights</h1>
       <div className={styles.list}>
-        {insights?.length
-          ? (
-            insights.map(({ id, text, date, brandId }) => (
-              <div className={styles.insight} key={id}>
-                <div className={styles["insight-meta"]}>
-                  <span>{brandId}</span>
-                  <div className={styles["insight-meta-details"]}>
-                    <span>{date.toString()}</span>
-                    <Trash2Icon
-                      className={styles["insight-delete"]}
-                      onClick={deleteInsight}
-                    />
-                  </div>
+        {insights?.length ? (
+          insights.map(({ id, text, createdAt, brand }) => (
+            <div className={styles.insight} key={id}>
+              <div className={styles['insight-meta']}>
+                <span>{brand}</span>
+                <div className={styles['insight-meta-details']}>
+                  <span>{createdAt}</span>
+                  <Trash2Icon
+                    className={styles['insight-delete']}
+                    onClick={deleteInsight}
+                  />
                 </div>
-                <p className={styles["insight-content"]}>{text}</p>
               </div>
-            ))
-          )
-          : <p>We have no insight!</p>}
+              <p className={styles['insight-content']}>{text}</p>
+            </div>
+          ))
+        ) : (
+          <p>We have no insight!</p>
+        )}
       </div>
     </div>
-  );
-};
+  )
+}
